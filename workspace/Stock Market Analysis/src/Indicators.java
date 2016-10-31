4e08483390485c78cd7a6cb286ef9701082ed65c
FrankFranck <frkfrck@gmail.com>
2016-10-31 00:35:11 -0400

    comments added

--- workspace/Stock Market Analysis/src/Indicators.java
+++ workspace/Stock Market Analysis/src/Indicators.java
@@ -4,8 +4,21 @@ import java.util.ArrayList;
 
 /**
  * Created by m.ding on 2016-10-30.
+ * <p>
+ * a class containing some indicators for buy/sell decision.
  */
 public class Indicators {
+    /**
+     * if the curve of shorter term moving average goes across
+     * the longer term moving from the bottom to the top, the
+     * intersection would be a signal for buying.
+     *
+     * @param shorterTermMovingAverages
+     * @param longerTermMovingAverages
+     * @return the index of the points where you should buy.
+     * index 0 of the return array is calculated from index 0
+     * of longerTermMovingAverages
+     */
     public static int[] buySignal(double[] shorterTermMovingAverages, double[] longerTermMovingAverages) {
         int subLength = longerTermMovingAverages.length;
         ArrayList resultArrayList = new ArrayList<Integer>();
@@ -26,6 +39,15 @@ public class Indicators {
         return ArrayOperations.convertToIntArray(resultArrayList);
     }
 
+    /**
+     * if the curve of shorter term moving average goes across
+     * the longer term moving from the bottom to the top, the
+     * intersection would be a signal for buying.
+     *
+     * @param shorterTermMovingAverages
+     * @param longerTermMovingAverages
+     * @return
+     */
     public static int[] sellSignal(double[] shorterTermMovingAverages, double[] longerTermMovingAverages) {
         int subLength = longerTermMovingAverages.length;
         ArrayList resultArrayList = new ArrayList<Integer>();
--- workspace/Stock Market Analysis/src/MovingAverages.java
+++ workspace/Stock Market Analysis/src/MovingAverages.java
@@ -2,9 +2,18 @@ import utility.ArrayOperations;
 
 /**
  * Created by m.ding on 2016-10-30.
+ * <p>
+ * to calculate different types of moving averages.
  */
-
 public class MovingAverages {
+    /**
+     * The most common method: Simple Moving Average (SMA)
+     *
+     * @param closingPrices - an array of historical data of closing prices
+     * @param periodInDays - could be 20 days, 50 days, 100 days or 200 days
+     * @return an array of simple moving average which
+     * starts at the index of (period - 1)
+     */
     public static double[] simpleMovingAverage(double[] closingPrices, int periodInDays) {
         int dataPoints = closingPrices.length;
         int resultLength = dataPoints - periodInDays;
@@ -18,7 +27,6 @@ public class MovingAverages {
     // public static double[] linearWeightedAverage() {} // not required in Iteration #1
 
     // public static double[] exponentialMovingAverage () {} // not required in Iteration #1
-
 
 
 }
